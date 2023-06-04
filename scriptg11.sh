2734fe75676016cd4ccf52aa6cb9c46ef9f57e0e
patgit12 <kochfox@gmail.com>
2023-06-04 18:07:03 +0000

    sending my new script online

---
+++ scriptg11.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo -n " provide the file name: "
+read filename
+echo "creating g11team and g11backup ..."
+mkdir g11team
+mkdir g11backup
+echo "creating the file  ... "
+cd g11team
+touch $filename
+echo "I'm currently a member of the group g11." > $filename
+cp $filename ../g11backup
+echo " showing the content of the file in g11team"
+cat $filename
+echo " showing the content of the file in g11backup"
+cat ../g11backup/$filename
+
