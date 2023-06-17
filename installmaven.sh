b374464f8d6ad624c222084b160c00de61ab3d9d
patgit12 <90884766+patgit12@users.noreply.github.com>
2023-06-17 18:30:23 -0400

    Script to install Maven

---
+++ installmaven.sh
@@ -0,0 +1,15 @@
+# install Java JDK 11+ as a pre-requisit for maven to run.
+sudo hostnamectl set-hostname maven
+#sudo su - ec2-user
+cd /opt
+sudo yum install wget nano tree unzip git -y
+sudo yum install java-11 -y
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.2/binaries/apache-maven-3.9.2-bin.zip
+sudo unzip apache-maven-3.9.2-bin.zip
+sudo rm -rf apache-maven-3.9.2-bin.zip
+sudo mv apache-maven-3.9.2/ maven
+cd
+echo 'export M2_HOME=/opt/maven' >> .bash_profile
+echo 'export PATH=$PATH:$M2_HOME/bin' >> .bash_profile
+source ~/.bash_profile
+mvn -version
