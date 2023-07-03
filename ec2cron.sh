e6be987880fa0ffc0e088c75f8eec8a6996a1342
patgit12 <90884766+patgit12@users.noreply.github.com>
2023-07-02 20:46:51 -0400

    Create ec2cron.sh

---
+++ ec2cron.sh
@@ -0,0 +1,8 @@
+# This installation is for Amazon Linux. Not sure it will work with RHEL
+sudo yum install cronie -y
+sudo systemctl start crond.service
+sudo systemctl enable crond.service
+echo "0 */4 * * * shutdown --poweroff +5 'The system is going down in five minutes.'" > cronjob001
+crontab -T cronjob001
+crontab < cronjob001
+crontab -l
