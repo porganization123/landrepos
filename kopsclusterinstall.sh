42d99cd8d8a749daeada15634cd68f0a119f0ec9
patgit12 <90884766+patgit12@users.noreply.github.com>
2023-08-22 14:51:56 -0400

    Update kopsclusterinstall.sh

--- kopsclusterinstall.sh
+++ kopsclusterinstall.sh
@@ -10,7 +10,7 @@ sudo apt install wget -y
 sudo wget https://github.com/kubernetes/kops/releases/download/v1.22.0/kops-linux-amd64
 sudo chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
-#install this if you are using a brand new EC2
+#install this if you are using a brand-new EC2
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
@@ -21,7 +21,7 @@ sleep 2m
 #AmazonS3FullAccess
 #IAMFullAccess 
 #AmazonVPCFullAccess
-aws s3 mb s3://class34kops # need to be deleted at the end
+aws s3 mb s3://class32kopspat # need to be deleted at the end
 aws s3 ls # to verify
 # Give Unique Name And S3 Bucket which you created.
 echo 'export NAME=class30.k8s.local' >> .bashrc
