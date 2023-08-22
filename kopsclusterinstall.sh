4df5ce3b11fab91e448fda257c6dacc9ca38e2b4
patgit12 <90884766+patgit12@users.noreply.github.com>
2023-08-22 17:13:32 -0400

    Update kopsclusterinstall.sh

--- kopsclusterinstall.sh
+++ kopsclusterinstall.sh
@@ -31,6 +31,7 @@ echo 'export NAME=class32.k8s.local' >> .bashrc
 echo 'export KOPS_STATE_STORE=s3://class32kopspat' >> .bashrc
 echo 'initializing the kops cluster'
 kops create cluster --zones us-east-1a --networking weave --master-size t2.medium --master-count 1 --node-size t2.micro --node-count=2 --name class32.k8s.local --state s3://class32kopspat
+echo 'UPDATE Cluster
 kops update cluster class32.k8s.local --yes
 sleep 6m
 kops export kubecfg class32.k8s.local --admin
