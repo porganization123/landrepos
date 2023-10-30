a3b614ae134b9a1dc0b89bc53b60905a18c985ca
patongit <90884766+patgit12@users.noreply.github.com>
2023-10-30 10:58:13 -0400

    Update newtom.sh

--- newtom.sh
+++ newtom.sh
@@ -1,17 +1,40 @@
 sudo curl https://tomcat.apache.org/download-90.cgi > latesttom
 
 default_vers=$(grep -o 'https://dlcdn[^"]*\.[0-9][0-9]\.zip' /home/ec2-user/latesttom | grep -o '[0-9].[0-9].[0-9][0-9]'| grep -m 1 [0-9].[0-9].[0-9][0-9])
-
+found=false
+while [ "$found" = "false" ]; do
 echo -n "what version are you planning to install [$default_vers]: "
 read tomvers
 url="https://archive.apache.org/dist/tomcat/tomcat-9/v$tomvers/bin/apache-tomcat-$tomvers.zip"
-while [ ! -z "$tomvers" ] && ( [ "$tomvers" != "$default_vers" ] || curl -s --head --fail "$url" | grep "Not Found" ); do
-    echo "Please provide a correct version! What your version number [$default_vers]: "
-    read tomvers
+
+if [ ! -z "$tomvers" ] && [ "$tomvers" != "$default_vers" ]; then
+   if curl -s --head --fail "$url"; then
+      found=true
+   else
+   echo "wrong"    
+   fi
+else
+   found=true
+fi
+
 done
+if [ -z "$tomvers" ] || [ "$tomvers" = "default_vers" ]; then
+if curl -s --head --fail "$url"; then
+      found=true
+   else
+   echo "wrong"    
+   fi
+else
+   found=true
+fi
+
+done
+if [ -z "$tomvers" ] || [ "$tomvers" = "default_vers" ]; then
+   wget https://dlcdn.apache.org/tomcat/tomcat-9/v$default_vers/bin/apache-tomcat-$default_vers.zip
+
+else
+   wget https://archive.apache.org/dist/tomcat/tomcat-9/v$tomvers/bin/apache-tomcat-$tomvers.zip
+
+fi
+
 echo "bingo"
-
-
-#if [ "$tomvers" = "9.0.82" ]; 
-#then echo "bingo" 
-#fi
