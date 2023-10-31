e052d9b84b09d828cf083eabbcd7ac0cb4b881fa
patongit <90884766+patgit12@users.noreply.github.com>
2023-10-31 01:14:29 -0400

    Update newtom.sh

--- newtom.sh
+++ newtom.sh
@@ -37,4 +37,45 @@ else
 
 fi
 
-echo "bingo"
+echo  -n "what's your desired username: "
+read username
+
+echo -n "provide your password please: "
+read password
+
+echo -n "desired role [separate each role by a coma]: "
+read role
+
+#####
+#Create Amazon Linux EC2 T2.micro Instance.
+# open port 8080 ..etc
+# change hostname to tomcat
+sudo hostnamectl set-hostname tomcat
+#sudo su - ec2-user
+sudo yum install git wget java-11 unzip cronie -y
+sudo curl https://tomcat.apache.org/download-90.cgi > latesttom
+cd /opt
+### Downloading latest tomcat ######
+sudo wget $(grep -o 'https://dlcdn[^"]*\.[0-9][0-9]\.zip' /home/ec2-user/latesttom)
+sudo unzip apache*.zip
+sudo rm -rf apache*.zip
+sudo rm -f /home/ec2-user/latesttom
+### rename tomcat for good naming convention
+sudo mv apache*[0-9] tomcat9  
+### assign executable permissions to the tomcat home directory
+sudo chmod 777 -R /opt/tomcat9
+sudo chown ec2-user -R /opt/tomcat9
+### start tomcat
+sh /opt/tomcat9/bin/startup.sh
+# create a soft link to start and stop tomcat
+sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
+starttomcat
+sudo sed -i '/^  <Valve*/i <!--' /opt/tomcat9/webapps/manager/META-INF/context.xml
+sudo sed -i '/^         allow*/a -->' /opt/tomcat9/webapps/manager/META-INF/context.xml
+sudo sed -i '/^<\/tomcat-users>/i <user username="$username" password="$password" roles="$roles"\/>' /opt/tomcat9/conf/tomcat-users.xml
+sudo systemctl start crond.service
+sudo systemctl enable crond.service
+echo "@reboot  starttomcat" > /opt/tomcat9/crontom
+crontab < /opt/tomcat9/crontom
+sudo su ec2-user
