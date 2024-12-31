325374b0bbb08d76cabdbd5164f81086f7b7163b
patongit <90884766+patgit12@users.noreply.github.com>
2024-12-31 08:40:21 -0500

    Update newsonar.sh

--- newsonar.sh
+++ newsonar.sh
@@ -6,13 +6,13 @@ sudo systemctl enable --now docker
 
 sudo usermod -aG docker ec2-user
 
-sysctl -w vm.max_map_count=524288
+sudo sysctl -w vm.max_map_count=524288
 
-sysctl -w fs.file-max=131072
+sudo sysctl -w fs.file-max=131072
 
-ulimit -n 131072
+sudo ulimit -n 131072
 
-ulimit -u 8192
+sudo ulimit -u 8192
 
 wget https://raw.githubusercontent.com/SonarSource/docker-sonarqube/refs/heads/master/example-compose-files/sq-with-postgres/docker-compose.yml
 
