aba559066a5585397cea872a90873f387807b125
patongit <90884766+patgit12@users.noreply.github.com>
2025-01-01 19:11:11 -0500

    Update InstallSonar.sh

--- InstallSonar.sh
+++ InstallSonar.sh
@@ -1,6 +1,6 @@
 # Size: t2medium for Rhel or t2small for amazonlinux
 # Amazonlinux: wget https://raw.githubusercontent.com/porganization123/landrepos/refs/heads/master/InstallSonar.sh && sh InstallSonar.sh
-# Rhel: sudo yum install wget -y &&  wget https://raw.githubusercontent.com/porganization123/landrepos/refs/heads/master/InstallSonar.sh && sh InstallSonar.sh
+# Rhel: curl -O https://raw.githubusercontent.com/porganization123/landrepos/refs/heads/master/InstallSonar.sh && sh InstallSonar.sh
 
 sudo useradd sonar
 # Grand sudo access to sonar user
